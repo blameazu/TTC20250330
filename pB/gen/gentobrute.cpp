e40612e554a36f4a408e4fe7bd66dd7d550285e7
blameazu <pig96922@gmail.com>
2025-03-21 18:21:23 +0800

    to fixed brute force can pass tests

--- pB/gen/gentobrute.cpp
+++ pB/gen/gentobrute.cpp
@@ -8,14 +8,11 @@ int main(int argc, char* argv[]) {
 
 	int q = atoi(argv[1]);
 	cout << q << '\n';
-	while(q--) {
-		int c = rnd.next(1, 3);
-		cout << c;
-		if(c == 1) {
-			cout << ' ' << rnd.next(1, INT_MAX);
-		}
-		cout << '\n';
-	}
+	for(int i = 0; i < q>>1; i++) {
+        cout << "1 87\n";
+    }
+    for(int i = 0; i < (q-(q>>1)); i++)
+        cout << "3\n";
 
 	return 0;
 }
