4abe95c3311a929b4f8df9e4e0aa62c50605659e
blameazu <pig96922@gmail.com>
2025-03-23 22:12:35 +0800

    Update correct.cpp

--- pA/solution/correct.cpp
+++ pA/solution/correct.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-set<pair<int, long long> > se;
-map<long long, int> mp;
+set<pair<int, int> > se;
+map<int, int> mp;
 void add(int x) {
-	if(mp[x]) se.erase({mp[x], x});
+	if(mp[x]) se.erase({mp[x], -x});
 	mp[x]++;
-	se.insert({mp[x], x});
+	se.insert({mp[x], -x});
 }
 void remove(int x) {
 	if(mp[x]) {
-		se.erase({mp[x], x});
+		se.erase({mp[x], -x});
 		mp[x]--;
-		se.insert({mp[x], x});
+		se.insert({mp[x], -x});
 	}
 }
 pair<int,int> query() {
 	if(se.empty()) return {-87, -87};
-	return {se.rbegin()->second, se.rbegin()->first};
+	return {-se.rbegin()->second, se.rbegin()->first};
 }
