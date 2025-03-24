84bda7d04e6c8b57f2dc5122b7139411dd764874
blameazu <pig96922@gmail.com>
2025-03-24 17:38:03 +0800

    pA

--- pA/validator/validator.cpp
+++ pA/validator/validator.cpp
@@ -1,13 +1,10 @@
 #include "testlib.h"
 using namespace std;
-
-const int MAXN = 1000;
-
 int main(int argc, char* argv[]) {
 	registerValidation(argc, argv);
 	// about testlib, see https://codeforces.com/blog/entry/18426
 
-	int q = inf.readInt(1, MAXN, "q");
+	int q = inf.readInt(1, 100000, "q");
 	inf.readEoln();
 	for(int i = 0; i < q; i++) {
 		int c = inf.readInt(1, 3, "c");
