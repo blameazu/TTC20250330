d93e5a2526b24894f3d4e78fc8af06f1af919a34
blameazu <pig96922@gmail.com>
2025-03-24 17:27:51 +0800

    grader

--- pA/grader/cpp/grader.cpp
+++ pA/grader/cpp/grader.cpp
@@ -1,16 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-static const string input_key = "ilovefrierenandfrierenlovemetoo";
-static const string output_key = "blameblameisblameblameandblameblameisnotblameblame";
+void add(int x);
+void remove(int x);
+int query();
 int main() {
     ios::sync_with_stdio, cin.tie(0);
-    {
-        string key; cin >> key;
-        if(key != intput_key) {
-            cout << output_key << "SV\n";
-            return 0;
-        }
-    }
     int q; cin >> q;
     while(q--) {
         int c; cin >> c;
@@ -22,6 +16,7 @@ int main() {
             remove(x);
         } else {
             int re = query();
+            cout << re << '\n';
         }
     }
 }
