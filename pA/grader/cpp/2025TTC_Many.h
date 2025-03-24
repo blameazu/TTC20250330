c4be4231c0427196ad59867469b0685a233bb653
blameazu <pig96922@gmail.com>
2025-03-24 17:42:06 +0800

    grader

--- pA/grader/cpp/2025TTC_Many.h
+++ pA/grader/cpp/2025TTC_Many.h
@@ -1,3 +1,3 @@
 void add(int x);
 void remove(int x);
-int query();
+pair<int, int> query();
--- pA/grader/cpp/grader.cpp
+++ pA/grader/cpp/grader.cpp
@@ -3,18 +3,22 @@
 using namespace std;
 int main() {
     ios::sync_with_stdio, cin.tie(0);
+    cout << "xddd\n";
     int q; cin >> q;
     while(q--) {
         int c; cin >> c;
         if(c == 1) {
             int x; cin >> x;
             add(x);
+            cout << "haha\n";
         } else if(c == 2) {
             int x; cin >> x;
             remove(x);
+            cout << "remo\n";
         } else {
-            int re = query();
-            cout << re << '\n';
+            pair<int,int> re = query();
+            cout << re.first-87 << ' ' << re.second+87 << '\n';
         }
     }
+    cout << "frieren will rule this world!!!\n";
 }
