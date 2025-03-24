9d5b0ecb9aa580d383a0e2d2f9e7b2b165da0e4f
blameazu <pig96922@gmail.com>
2025-03-24 19:10:35 +0800

    hahaha

--- pB/gen/gentolog2.cpp
+++ pB/gen/gentolog2.cpp
@@ -9,8 +9,10 @@ int main(int argc, char* argv[]) {
 	int q = atoi(argv[1]);
 	cout << q << '\n';
 	int pu = q*argv[2]/100;
+	cout << "1 88\n";
 	for(int i = 0; i < pu; i++) cout << "1 87\n";
-	for(int i = 0; i < q-pu; i++) cout << "3\n";
+	for(int i = 0; i < q-3-pu; i++) cout << "3\n";
+	cout << "2\n3\n";
 
 	return 0;
 }
