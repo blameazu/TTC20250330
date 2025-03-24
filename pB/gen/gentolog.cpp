cf403e3f63aa9b8c6a38b46143c78207690f70b6
blameazu <pig96922@gmail.com>
2025-03-24 19:05:36 +0800

    update

---
+++ pB/gen/gentolog.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	registerGen(argc, argv, 1);
+	// about testlib, see https://codeforces.com/blog/entry/18291
+
+	int q = atoi(argv[1]);
+	cout << q << '\n';
+	int pu = q*95/100;
+	for(int i = 0; i < pu; i++) cout << "1 87\n";
+	for(int i = 0; i < q-pu; i++) cout << "3\n";
+
+	return 0;
+}
